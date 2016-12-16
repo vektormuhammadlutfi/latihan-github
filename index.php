d50b9c1285aacaa52fd8a3d1a0045866d1a5986f
Muhammad lutfi <sainteclutfi@gmail.com>
2016-12-16 14:21:26 +0800

    saya rubah jadi login

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>buat login</title>
+	<title>Login Twitter</title>
 	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
 </head>
 <style>
